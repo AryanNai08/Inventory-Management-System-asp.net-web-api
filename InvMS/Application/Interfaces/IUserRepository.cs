94b29a7e167a904523e11b7b6e34b6f4a6c565b9
Nai Aryan <130205139+AryanNai08@users.noreply.github.com>
2026-03-02 14:58:23 +0530

    implemeted user interface and userrepository

---
+++ InvMS/Application/Interfaces/IUserRepository.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<List<User>> GetAllAsync();
+        Task<User> GetByIdAsync(int id);
+
+        Task<User> GetByUsernameAsync(string username);
+
+        Task AddAsync(User user);
+
+        Task UpdateAsync(User user);
+
+        Task SoftDeleteAsync(int id);
+
+    }
+}
+  
---
+++ InvMS/Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,58 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly InventoryDbContext _dbContext;
+        public UserRepository(InventoryDbContext dbContext) 
+        {
+            _dbContext=dbContext;
+        }
+        public async Task AddAsync(User user)
+        {
+             await _dbContext.AddAsync(user);
+
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<List<User>> GetAllAsync()
+        {
+            return await _dbContext.Users.Where(u => !u.IsDeleted).ToListAsync();
+        }
+
+        public async Task<User> GetByIdAsync(int id)
+        {
+            return await _dbContext.Users.Where(u => u.Id == id && !u.IsDeleted).FirstOrDefaultAsync();
+        }
+
+        public async Task<User> GetByUsernameAsync(string username)
+        {
+            return await _dbContext.Users.Where(u => u.Username == username).FirstOrDefaultAsync();
+        }
+
+        public async Task SoftDeleteAsync(int id)
+        {
+            var user = await _dbContext.Users.FirstOrDefaultAsync(u => u.Id == id && !u.IsDeleted);
+            if (user == null)
+                return;
+            user.IsDeleted = true;
+            user.ModifiedDate = DateTime.Now;
+            _dbContext.Users.Update(user);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(User user)
+        {
+            _dbContext.Users.Update(user);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
