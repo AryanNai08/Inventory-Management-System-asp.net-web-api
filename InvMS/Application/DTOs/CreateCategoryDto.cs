01ab1d98a8df01274973c417c39b9ab624e5bd08
Nai Aryan <130205139+AryanNai08@users.noreply.github.com>
2026-03-05 10:47:47 +0530

    (module 2) :- Created category related dtos

---
+++ InvMS/Application/DTOs/CreateCategoryDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Application.DTOs
+{
+    public class CreateCategoryDto
+    {
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ InvMS/Application/DTOs/UpdateCategoryDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Application.DTOs
+{
+    public class UpdateCategoryDto
+    {
+        
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
+
