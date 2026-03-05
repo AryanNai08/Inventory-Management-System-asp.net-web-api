855802d0e7bd549ce2762ce46342d2a24ca920e3
Nai Aryan <130205139+AryanNai08@users.noreply.github.com>
2026-03-05 11:33:27 +0530

    (Category Controller) :- Implemented category controller and its endpoints

---
+++ InvMS/InvMS/Controller/CategoriesController.cs
@@ -0,0 +1,109 @@
+﻿using Application.Common;
+using Application.DTOs;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace InvMS.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+
+        private readonly ICategoryService _categoryService;
+        private readonly APIResponse _apiResponse;
+        public CategoriesController(ICategoryService categoryService, APIResponse apiResponse)
+        {
+            _apiResponse = apiResponse;
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        [Route("GetAllCategories")]
+        [Authorize]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<APIResponse>> GetAllCategories()
+        {
+            _apiResponse.Data = await _categoryService.GetAllAsync();
+            _apiResponse.StatusCode = HttpStatusCode.OK;
+            _apiResponse.Status = true;
+
+            return Ok(_apiResponse);
+        }
+
+        [HttpGet]
+        [Route("{id:int}",Name="GetCategoryById")]
+        [Authorize]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<APIResponse>> GetCategoryById(int id)
+        {
+            _apiResponse.Data = await _categoryService.GetByIdAsync(id);
+            _apiResponse.StatusCode = HttpStatusCode.OK;
+            _apiResponse.Status = true;
+
+            return Ok(_apiResponse);
+        }
+
+        [HttpPost]
+        [Route("CreateCategory")]
+        [Authorize(Roles ="Admin")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<APIResponse>> CreateCategory([FromBody] CreateCategoryDto dto)
+        {
+            _apiResponse.Data = await _categoryService.CreateAsync(dto);
+            _apiResponse.StatusCode = HttpStatusCode.Created;
+            _apiResponse.Status = true;
+
+            return CreatedAtRoute("GetCategoryById", dto);
+        }
+
+        [HttpPut]
+        [Route("{id:int}",Name ="UpdateCategory")]
+        [Authorize(Roles = "Admin")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<APIResponse>> UpdateCategory(int id,UpdateCategoryDto dto)
+        {
+            _apiResponse.Data = await _categoryService.UpdateAsync(id,dto);
+            _apiResponse.StatusCode = HttpStatusCode.OK;
+            _apiResponse.Status = true;
+
+            return Ok(_apiResponse);
+        }
+
+        [HttpPut]
+        [Route("{id:int}", Name = "DeleteCategory")]
+        [Authorize(Roles = "Admin")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<APIResponse>> DeleteCategory(int id)
+        {
+            _apiResponse.Data = await _categoryService.SoftDeleteAsync(id);
+            _apiResponse.StatusCode = HttpStatusCode.OK;
+            _apiResponse.Status = true;
+
+            return Ok(_apiResponse);
+        }
+
+
+    }
+}
