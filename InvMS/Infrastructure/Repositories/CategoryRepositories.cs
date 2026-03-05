496f87d70c3a43deab64bd3feaacb398d907a98a
Nai Aryan <130205139+AryanNai08@users.noreply.github.com>
2026-03-05 10:50:21 +0530

    (Repository) :- Implemented category repository

---
+++ InvMS/Infrastructure/Repositories/CategoryRepositories.cs
@@ -0,0 +1,59 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Domain.Exceptions;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Repositories
+{
+    public class CategoryRepositories : ICategoryRepository
+    {
+        private readonly InventoryDbContext _dbContext;
+
+        public CategoryRepositories(InventoryDbContext dbContext) 
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task AddAsync(Category category)
+        {
+            await _dbContext.AddAsync(category);
+
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<List<Category>> GetAllAsync()
+        {
+          return  await _dbContext.Categories.Where(c=>!c.IsDeleted).ToListAsync();
+        }
+
+        public async Task<Category> GetByIdAsync(int id)
+        {
+            return await _dbContext.Categories.Where(c =>c.Id==id && !c.IsDeleted).FirstOrDefaultAsync();
+        }
+
+        public async Task<Category> GetByNameAsync(string name)
+        {
+            return await _dbContext.Categories.Where(c => c.Name == name && !c.IsDeleted).FirstOrDefaultAsync();
+        }
+
+        public async Task SoftDeleteAsync(int id)
+        {
+            var category= await _dbContext.Categories.Where(c => c.Id == id && !c.IsDeleted).FirstOrDefaultAsync();
+            if (category == null)
+                throw new NotFoundException($"User with id:{id} not found");
+            category.IsDeleted=true;
+            category.ModifiedDate= DateTime.Now;
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Category category)
+        {
+            _dbContext.Categories.Update(category);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
