f33db44e9d196acae9a4d9afe897ec8229241d48
Nai Aryan <130205139+AryanNai08@users.noreply.github.com>
2026-03-13 15:41:51 +0530

    (deployment) :- changes regarding deployment (swagger)

--- InvMS/InvMS/Program.cs
+++ InvMS/InvMS/Program.cs
@@ -107,15 +107,22 @@ var app = builder.Build();
 app.UseMiddleware<ExceptionMiddleware>();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI(c =>
+//    {
+//        c.SwaggerEndpoint("/swagger/v1/swagger.json", "InvMS v1");
+//        c.RoutePrefix = string.Empty;
+//    });
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "InvMS v1");
-        c.RoutePrefix = string.Empty;
-    });
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "InvMS v1");
+    c.RoutePrefix = string.Empty;
+});
 
 
 
